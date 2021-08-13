fa2c1f40bbdaf3e26e4bbd883c9b2482fcfcd986
Oskar Rosén <oskar@oskarrosen.com>
2021-08-13 23:29:38 +0200

    replace yargs with arg for parsing argv to start script

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,36 @@
-import { argv } from 'yargs'
+import arg from 'arg'
 
 import { generateCalendar } from './calendar'
 
-if (argv.cs) {
+const args = arg({
+  '--cs': Boolean,
+  '--nba': Boolean,
+  '--football': Boolean,
+})
+
+if (!args['--cs'] && !args['--nba'] && !args['--football']) {
+  console.log(`Please provide a valid argument!
+
+--cs, --nba, --football`)
+}
+
+if (args['--cs']) {
   ;(async () => {
     const result = await generateCalendar('CS')
     console.log('result', result)
   })()
 }
 
-if (argv.nba) {
+if (args['--nba']) {
   ;(async () => {
     const result = await generateCalendar('NBA')
     console.log('result', result)
   })()
 }
 
-if (argv.football) {
+if (args['--football']) {
   ;(async () => {
     const result = await generateCalendar('Football')
     console.log('result', result)
   })()
-}
-
-if (!argv.cs && !argv.nba && !argv.football) {
-  console.log(`Please provide a valid argument!
-
---cs, --nba, --football`)
 }
